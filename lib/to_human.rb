9f80e5c3dce3e65941387db0ae02f3d6b5b33656
Florinel Gorgan <florin@floringorgan.com>
2018-05-31 16:56:38 +0300

    Initial commit

---
+++ lib/to_human.rb
@@ -0,0 +1,5 @@
+require "to_human/version"
+
+module ToHuman
+  # Your code goes here...
+end
---
+++ spec/to_human_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe ToHuman do
+  it "has a version number" do
+    expect(ToHuman::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
